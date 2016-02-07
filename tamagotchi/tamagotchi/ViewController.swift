0536d522085dc51b8347cd9a1bcded67b02cc0bd
Selby <selby@Selbys-MacBook-Air.local>
2016-02-07 01:05:50 -0500

    typefaces added

--- tamagotchi/tamagotchi/ViewController.swift
+++ tamagotchi/tamagotchi/ViewController.swift
@@ -10,17 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        let Gif = UIImage.gifWithName("fat")
-//        let imageView = UIImageView(image: Gif)
-//        imageView.frame = CGRect(x: 40.0, y: 20.0, width: 316.0, height: 517.0)
-//        
-//        view.addSubview(imageView)
+        let imageName = "Full Background .png"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+        view.addSubview(imageView)
+        
+        
+        
+        let Gif = UIImage.gifWithName("fat")
+        let imageView1 = UIImageView(image: Gif)
+        imageView1.frame = CGRect(x: 40.0, y: 20.0, width: 316.0, height: 517.0)
+        
+        view.addSubview(imageView1)
+        
+        
         
         let Gif2 = UIImage.gifWithName("skinny")
         let imageView2 = UIImageView(image: Gif2)
@@ -28,11 +38,24 @@ class ViewController: UIViewController {
         
         view.addSubview(imageView2)
         
+        let textFiled = UITextField(frame: CGRectMake(20.0, 30.0, 100.0, 33.0))
+        textFiled.backgroundColor = UIColor.redColor()
+        textFiled.borderStyle = UITextBorderStyle.Line
+        self.view.addSubview(textFiled)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return [UIInterfaceOrientationMask.Portrait, UIInterfaceOrientationMask.PortraitUpsideDown]
     }
 
 
