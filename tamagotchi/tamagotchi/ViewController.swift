93a8ad43523b1eb66ede9f7c6737dc71bae982fb
Selby <selby@Selbys-MacBook-Air.local>
2016-02-07 00:30:16 -0500

    gif working

--- tamagotchi/tamagotchi/ViewController.swift
+++ tamagotchi/tamagotchi/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let Gif = UIImage.gifWithName("fat")
+//        let imageView = UIImageView(image: Gif)
+//        imageView.frame = CGRect(x: 40.0, y: 20.0, width: 316.0, height: 517.0)
+//        
+//        view.addSubview(imageView)
+        
+        let Gif2 = UIImage.gifWithName("skinny")
+        let imageView2 = UIImageView(image: Gif2)
+        imageView2.frame = CGRect(x: 60.0, y: 20.0, width: 278.0, height: 517.0)
+        
+        view.addSubview(imageView2)
+        
     }
 
     override func didReceiveMemoryWarning() {
