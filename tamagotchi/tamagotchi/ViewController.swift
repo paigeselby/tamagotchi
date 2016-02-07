d2ee8f07d0fe9a780d4cddce93dada788c19e177
Selby <selby@Selbys-MacBook-Air.local>
2016-02-07 01:46:00 -0500

    welcome page added

--- tamagotchi/tamagotchi/ViewController.swift
+++ tamagotchi/tamagotchi/ViewController.swift
@@ -15,36 +15,48 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let imageName = "Full Background .png"
+//        let imageName = "Full Background .png"
+//        let image = UIImage(named: imageName)
+//        let imageView = UIImageView(image: image!)
+//        
+//        imageView.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+//        view.addSubview(imageView)
+//        
+//        
+//        
+//        let Gif = UIImage.gifWithName("fat")
+//        let imageView1 = UIImageView(image: Gif)
+//        imageView1.frame = CGRect(x: 140.0, y: 150.0, width: 252.8, height: 413.6)
+//        
+////        view.addSubview(imageView1)
+//        
+//        
+//        
+//        let Gif2 = UIImage.gifWithName("skinny")
+//        let imageView2 = UIImageView(image: Gif2)
+//        imageView2.frame = CGRect(x: 20.0, y: 150.0, width: 222.4, height: 413.6)
+//        
+////        view.addSubview(imageView2)
+//        
+//        let textFiled = UITextField(frame: CGRectMake(20.0, 30.0, 200.0, 33.0))
+//        textFiled.font = UIFont(name: "Minecraft", size: 40)
+//        textFiled.text = "Hi Emily & Katie!"
+//        textFiled.backgroundColor = UIColor.whiteColor()
+////        textFiled.borderStyle = UITextBorderStyle.Line
+//        self.view.addSubview(textFiled)
+        
+        
+        let imageName = "Welcome Page.png"
         let image = UIImage(named: imageName)
         let imageView = UIImageView(image: image!)
         
         imageView.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
         view.addSubview(imageView)
         
+        let secondViewController = self.storyboard?.instantiateViewControllerWithIdentifier("LoginViewController") as LoginViewController
+        self.navigationController?.pushViewController(secondViewController, animated: true)
         
-        
-        let Gif = UIImage.gifWithName("fat")
-        let imageView1 = UIImageView(image: Gif)
-        imageView1.frame = CGRect(x: 140.0, y: 150.0, width: 252.8, height: 413.6)
-        
-        view.addSubview(imageView1)
-        
-        
-        
-        let Gif2 = UIImage.gifWithName("skinny")
-        let imageView2 = UIImageView(image: Gif2)
-        imageView2.frame = CGRect(x: 20.0, y: 150.0, width: 222.4, height: 413.6)
-        
-        view.addSubview(imageView2)
-        
-        let textFiled = UITextField(frame: CGRectMake(20.0, 30.0, 200.0, 33.0))
-        textFiled.font = UIFont(name: "Minecraft", size: 40)
-        textFiled.text = "Hi Emily & Katie!"
-        textFiled.backgroundColor = UIColor.whiteColor()
-//        textFiled.borderStyle = UITextBorderStyle.Line
-        self.view.addSubview(textFiled)
-        
+
     }
 
     override func didReceiveMemoryWarning() {
